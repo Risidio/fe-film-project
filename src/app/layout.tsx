67a482921a8f16f09575b5f97a8e2adef94df72c
TanvirDF <ahamedtanvir619@gmail.com>
2025-03-17 17:17:37 +0000

    home page displays features available

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar/>
-
         {children}
       </body>
     </html>
---
+++ src/app/smart-contracts/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="mt-20 font-bold flex flex-col items-center justify-center">
+           smart contracts
+        </div>
+    )
+}
