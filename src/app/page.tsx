9252ba756b49bbb22d7863205fc66a049e51a057
TanvirDF <ahamedtanvir619@gmail.com>
2025-04-03 16:20:24 +0100

    fixed error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,3 +39,59 @@ export default function Home() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // This is a MVP project created by unpaid interns AKA free slaves for
+            // Risidio who did not pay us for our work nor valued it. If you see
+            // this, most probably you are the new batch of slaves who will be
+            // working on this project. We are sorry for you if you have been
+            // misguided like us. Better leave asap.
--- src/components/AssetSelection.tsx
+++ src/components/AssetSelection.tsx
@@ -6,6 +6,55 @@
 export default AssetSelection;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             // This is a MVP project created by unpaid interns AKA free slaves for
             // Risidio who did not pay us for our work nor valued it. If you see
             // this, most probably you are the new batch of slaves who will be
