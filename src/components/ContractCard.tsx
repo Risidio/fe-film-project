c752d6de25e69246d04bc11802c3940a6c4fbee8
TanvirDF <ahamedtanvir619@gmail.com>
2025-04-03 16:19:13 +0100

    fixed error

--- src/components/ContractCard.tsx
+++ src/components/ContractCard.tsx
@@ -41,41 +41,6 @@ export function SmartContractCard({
   chainSupport,
 }: ContractProps) {
   return (
-    <Card className="h-fit transition-shadow duration-300 hover:shadow-lg">
-      <CardHeader>
-        <div className="mb-5">
-          <Tag text={type} colour="text-blue-500" />
-        </div>
-        <CardTitle className="text-xl">{title}</CardTitle>
-        <CardDescription className="h-10">{description}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="h-32 items-center justify-between border-t-2 py-6">
-          <p className="mb-2 text-sm font-semibold">Key functions:</p>
-          <div className="w-fit-content flex flex-wrap gap-2">
-            {functions.map((func) => (
-              <Tag key={uuidv4()} text={func} />
-            ))}
-          </div>
-        </div>
-      </CardContent>
-      <CardFooter className="mt-10 flex items-center justify-between">
-        <ContractModal
-          title={title}
-          type={type}
-          description={description}
-          keyFeatures={keyFeatures}
-          useCases={useCases}
-          standards={standards}
-          chainSupport={chainSupport}
-        />
-        <Link href={`/smart-contracts/${encodeURIComponent(title)}`} passHref>
-          <Button>
-            <IoRocketOutline />
-            Deploy
-          </Button>
-        </Link>
-      </CardFooter>
-    </Card>
+ 
   );
 }
